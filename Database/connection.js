6bebfafb5794ae9cecb20df69a49e535b039f163
praveen-raj-143 <praveenrajgreath@gmail.com>
2023-08-03 15:20:40 +0530

    content add

--- Database/connection.js
+++ Database/connection.js
@@ -51,6 +51,6 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery",true);
 
-const connection = mongoose.connect("mongodb+srv://praveenraj:m5qDCTekCsGRigeU@cluster0.0vpqaam.mongodb.net/Movies?retryWrites=true&w=majority")
+const connection = mongoose.connect("mongodb+srv://praveenraj:s9jnBNgCwF81rep9@cluster0.0vpqaam.mongodb.net/Movies?retryWrites=true&w=majority")
 console.log("connected to database")
 module.exports= connection;
